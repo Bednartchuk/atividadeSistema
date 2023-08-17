d3dd8b2e2711e8eae859b47129519298f18aeab4
Bednartchuk <125326592+Bednartchuk@users.noreply.github.com>
2023-08-17 20:55:18 -0300

    Add files via upload

---
+++ atividade3.py
@@ -0,0 +1,14 @@
+class SistemaOperacional:
+    def __init__(self, nome, versao):
+        self.nome = nome
+        self.versao = versao
+
+class Computador:
+    def __init__(self, sistema):
+        self.sistema = sistema
+
+sistema_windows = SistemaOperacional(nome="Windows", versao="10")
+meu_computador = Computador(sistema=sistema_windows)
+
+print(f"{meu_computador.sistema.nome} {meu_computador.sistema.versao}")
+ 
